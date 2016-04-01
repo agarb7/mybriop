a1736d343aa1e7082c33841b61e7d15f90aaf152
= <=>
2016-04-01 12:43:49 +0800

    фикс статуса курса

--- views/sotrudnik-att-komissii/index.php
+++ views/sotrudnik-att-komissii/index.php
@@ -1,6 +1,7 @@
 <?php
 
 use \app\helpers\Html;
+$this->title = 'Оценивание аттестующихся';
 
 $this->registerJsFile('/js/angular.min.js');
 $this->registerJsFile('/js/sotrudnikAttKomissii.js');
@@ -15,29 +16,33 @@ foreach ($periods as $period) {
 ?>
 <div ng-app="otsenki">
 
-<div class="inline-block" ng-controller="SpisokController as s">
-    <div class="inline-block">
-        <?=Html::label('Период прохождения аттестации','periods',[]);?>
-        <?=Html::dropDownList('periods',null,$periods_for_dropdown,['id'=>'periods','class'=>'form-control inline-block','ng-model'=>'s.period']);?>
+    <div class="inline-block" ng-controller="SpisokController as s">
+        <div class="inline-block">
+            <?=Html::label('Период прохождения аттестации','periods',[]);?>
+            <?=Html::dropDownList('periods',null,$periods_for_dropdown,['id'=>'periods','class'=>'form-control inline-block','ng-model'=>'s.period']);?>
+        </div>
+        <div class="inline-block relative" style="top: -1px">
+            <?=Html::button('Загрузить список заявлений',['class'=>'btn btn-primary','ng-click'=>'s.loadZayavleniya()'])?>
+        </div>
+        <p></p>
+        <div>
+            <table ng-class="s.spisok.length > 0 ? '' : 'hidden' " class="tb">
+                <tr class="thead">
+                    <td>ФИО</td>
+                    <td>Должность</td>
+                    <td></td>
+                </tr>
+                 <tr ng-repeat="item in s.spisok">
+                     <td>{{item.familiya+' '+item.imya+' '+item.otchestvo}}</td>
+                     <td>{{item.organizaciyaRel.nazvanie+', '+item.dolzhnostRel.nazvanie}}</td>
+                     <td><button class="btn btn-primary" ng-click="s.putMarks(item.id)">Поставить оценки</button></td>
+                 </tr>
+            </table>
+        </div>
     </div>
-    <div class="inline-block relative" style="top: -1px">
-        <?=Html::button('Загрузить список заявлений',['class'=>'btn btn-primary','ng-click'=>'s.loadZayavleniya()'])?>
+
+    <div ng-controller="OtsenkiController as o">
+
     </div>
-    <p></p>
-    <div>
-        <table ng-class="s.spisok.length > 0 ? '' : 'hidden' " class="tb">
-            <tr class="thead">
-                <td>ФИО</td>
-                <td>Должность</td>
-                <td></td>
-            </tr>
-             <tr ng-repeat="item in s.spisok">
-                 <td>{{item.familiya+' '+item.imya+' '+item.otchestvo}}</td>
-                 <td>{{item.organizaciyaRel.nazvanie+', '+item.dolzhnostRel.nazvanie}}</td>
-                 <td><button class="btn btn-primary" ng-click="s.putMarks(item.id)">Поставить оценки</button></td>
-             </tr>
-        </table>
-    </div>
-</div>
 
 </div>
