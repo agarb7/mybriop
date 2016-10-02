da5cd3c397a2ff8f76e837575e595029ea570dfc
agarb7 <agarb7@gmail.com>
2016-10-02 18:34:51 +0800

    https://trello.com/c/PRji6Wet/139--

--- controllers/KursSlushatelyuController.php
+++ controllers/KursSlushatelyuController.php
@@ -6,6 +6,7 @@ use app\entities\Kim;
 use app\entities\KursExtended;
 use app\enums\Rol;
 use app\enums\TipKursa;
+use app\enums2\StatusProgrammyKursa;
 use app\helpers\Hashids;
 use app\models\kurs_slushatelyu\InfoOIupForm;
 use app\models\kurs_slushatelyu\SpisokKursovFilterForm;
@@ -30,6 +31,9 @@ class KursSlushatelyuController extends Controller
 
         if ($kursRecord->getAvailableAction()[0] !== KursExtended::AVAILABLE_ACTION_PROGRAMMA)
             throw new HttpException(422);
+
+        if ($kursRecord->statusProgrammy !== StatusProgrammyKursa::ZAVERSHENA)
+            return $this->render('programma-kursa-isnt-ready', compact('kursRecord'));
 
         return $this->render('programma-kursa', compact('kursRecord'));
     }
