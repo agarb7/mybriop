b630bf9adaddd7da5f616cb3597b800f715ee6c8
asv <svarkh@mail.ru>
2017-12-13 16:56:19 +0800

    otchet_po_IK_s_trebovaniem_pereotsenki

--- modules/attestaciya_otchety/views/list/ik.php
+++ modules/attestaciya_otchety/views/list/ik.php
@@ -1,18 +1,6 @@
 <?
 use app\enums\KategoriyaPedRabotnika;
-?>
 
-<h3>Оценочный лист ИК</h3>
-<p>Ф.И.О. аттестуемого педагогического работника: <?=$zayavlenie->getFio()?></p>
-<p>Образовательное учреждение, место работы (полное официальное наименование): <?=$zayavlenie->organizaciyaRel->nazvanie?></p>
-<p>Должность: <?=$zayavlenie->dolzhnostRel->nazvanie?></p>
-<p>Стаж работы в данной должности: <?=$zayavlenie->stazh_v_dolzhnosti?></p>
-<p>Имеющая квалификационная категория, срок действия: <?= KategoriyaPedRabotnika::namesMap()[$zayavlenie->attestaciya_kategoriya];
-   if(!($zayavlenie->attestaciya_kategoriya == 'bez_kategorii')) echo", $zayavlenie->attestaciya_data_okonchaniya_dejstviya"; ?>
-</p>
-<p>Аттестация на: <?=KategoriyaPedRabotnika::namesMap()[$zayavlenie->na_kategoriyu]?></p>
-
-<?
 $i = 0; // nomer otsenochnogo lista zayvleniya
 $colpocazateley = 0; // chislo pokazateley otsenochnogo lista zayvleniya
 $colexpertiz =0;
@@ -21,6 +9,7 @@ foreach ($list as $otsenochnyjList) {
     $komissiya[] = $otsenochnyjList->rabotnikKomissiiFizLicoRel->getFio();
     $s = 0;
     foreach ($otsenochnyjList->strukturaOtsenochnogoListaZayvaleniyaRel as $structura){
+        //var_dump($structura);die();
         if($i == 0){
             $nomer[] = $structura->nomer;
             $nazvanie[] = $structura->nazvanie;
@@ -41,29 +30,41 @@ foreach ($list as $otsenochnyjList) {
 }
 //var_dump($bally);die();
 
-$colexp = $i;
-for($i=0;$i<$colpocazateley;$i++){
-    $s=0;
-    for($j=0;$j<$colexp;$j++){
-        $s +=$bally[$j][$i];
+if ($colexpertiz>0){
+    $colexp = $i;
+    for($i=0;$i<$colpocazateley;$i++){
+        $s=0;
+        for($j=0;$j<$colexp;$j++){
+            //if (isset($bally[$j][$i]))$s +=$bally[$j][$i];
+            $s +=$bally[$j][$i];
+        }
+        $rezultat[] = number_format($s/$colexpertiz,2);
     }
-    $rezultat[] = number_format($s/$colexpertiz,2);
-}
-?>
+    ?>
 
-<table class="tb">
-    <thead>
-    <tr>
-        <td class="center">№</td>
-        <td>Показатели для оценки</td>
-        <td>Максимальное значение</td>
-        <?$k=1; for ($i=0;$i<$colexp;$i++){
-            if($total[$i]>0):echo "<td class=\"center\">Эксперт $k</td>";$k++;endif;
-        } ?>
-        <td>Результаты оценки</td>
-    </tr>
-    </thead>
-    <tbody>
+<h3>Оценочный лист ИК</h3>
+<p>Ф.И.О. аттестуемого педагогического работника: <?=$zayavlenie->getFio()?></p>
+<p>Образовательное учреждение, место работы (полное официальное наименование): <?=$zayavlenie->organizaciyaRel->nazvanie?></p>
+<p>Должность: <?=$zayavlenie->dolzhnostRel->nazvanie?></p>
+<p>Стаж работы в данной должности: <?=$zayavlenie->stazh_v_dolzhnosti?></p>
+<p>Имеющая квалификационная категория, срок действия: <?= KategoriyaPedRabotnika::namesMap()[$zayavlenie->attestaciya_kategoriya];
+    if(!($zayavlenie->attestaciya_kategoriya == 'bez_kategorii')) echo", $zayavlenie->attestaciya_data_okonchaniya_dejstviya"; ?>
+</p>
+<p>Аттестация на: <?=KategoriyaPedRabotnika::namesMap()[$zayavlenie->na_kategoriyu]?></p>
+
+    <table class="tb">
+        <thead>
+        <tr>
+            <td class="center">№</td>
+            <td>Показатели для оценки</td>
+            <td>Максимальное значение</td>
+            <?$k=1; for ($i=0;$i<$colexp;$i++){
+                if($total[$i]>0):echo "<td class=\"center\">Эксперт $k</td>";$k++;endif;
+            } ?>
+            <td>Результаты оценки</td>
+        </tr>
+        </thead>
+        <tbody>
         <? foreach ($nazvanie as $index=>$value): ?>
             <tr>
                 <td class="center"><?=$nomer[$index]?></td>
@@ -79,12 +80,14 @@ for($i=0;$i<$colpocazateley;$i++){
             <?for($i=0;$i<$colexp;$i++):if($total[$i]>0):?><td class="center"><?=$total[$i]?></td><?endif;endfor;?>
             <td class="center"><?=number_format(array_sum($total)/$colexpertiz,2)?></td>
         </tr>
-    </tbody>
-</table>
-<br>
-<p>Дата: "_____"_________________20___г.</p>
-<br>
-<? $k=1; for($i=0;$i<$colexp;$i++):$n=$i+1;if($total[$i]>0):?>
-    <p><?=$k?> Эксперт: __________________________________ / <?=$komissiya[$i]?><br>
-    &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;<sup>подпись</sup><p>
-<?$k++;endif;endfor; ?>
+        </tbody>
+    </table>
+    <br>
+    <p>Дата: "_____"_________________20___г.</p>
+    <br>
+    <? $k=1; for($i=0;$i<$colexp;$i++):$n=$i+1;if($total[$i]>0):?>
+        <p><?=$k?> Эксперт: __________________________________ / <?=$komissiya[$i]?><br>
+            &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;<sup>подпись</sup><p>
+        <?$k++;endif;endfor; ?>
+<?} else echo"<h3>Оценочные листы ИК не имеют оценок!!!</h3> Для формирования нового оценочного листа ИК эксперта требуется удалить все неправильно оцененные листы и провести повторную экспертизу.";?>
+
