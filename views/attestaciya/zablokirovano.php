4f3aeef99915337bd9ba7af374794364ac9ff994
asv <svarkh@mail.ru>
2018-02-14 09:24:37 +0800

    fix_uvedomlenie_o_lock_zayavleniya_na_attestaciju

--- views/attestaciya/zablokirovano.php
+++ views/attestaciya/zablokirovano.php
@@ -2,5 +2,5 @@
 
 echo '<p>Уважаемый(ая) '.$model->familiya.' '.$model->imya.' '.$model->otchestvo.'!</p>';
 echo '<p>Аттестационная комиссия МОиН РБ отклонила Ваше заявление, поданное по должности '.$model->dolzhnostRel['nazvanie'].', '.$model->organizaciyaRel['nazvanie'].' для прохождения аттестации с '.\Yii::$app->formatter->asDate($model->vremyaProvedeniyaAttestaciiRel->nachalo,'php:d.m.Y').
-    ' по '.\Yii::$app->formatter->asDate($model->vremyaProvedeniyaAttestaciiRel->konec,'php:d.m.Y').'. '.$comment.'</p>';
+    ' по '.\Yii::$app->formatter->asDate($model->vremyaProvedeniyaAttestaciiRel->konec,'php:d.m.Y').' гг. в связи с тем, что '.$comment.'</p>';
 echo '<p>Отдел аттестации и РПК.</p>';
